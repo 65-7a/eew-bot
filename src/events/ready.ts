bd4a77a58b70a26da0ce788ea68889f695c0a6ec
callum-wong <exploit.exploit.02@gmail.com>
2022-03-30 12:39:05 +1100

    Add commit revision to status

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -2,9 +2,14 @@ import { client, logger, p2pQuake } from "..";
 import { Event } from "../structures/Event";
 
 export default new Event("ready", async () => {
+    const revision = (await import("child_process"))
+        .execSync("git rev-parse --short HEAD")
+        .toString()
+        .trim();
+
     client.user.setActivity({
         type: "WATCHING",
-        name: "for earthquakes"
+        name: `for earthquakes | ${revision}`
     });
 
     logger.log("success", `Logged in as ${client.user.tag}`);
