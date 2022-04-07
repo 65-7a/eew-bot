2d5eaef7542d35edd8b244ce2fccde3cab62bc1f
65_7a <exploit.exploit.02@gmail.com>
2022-04-07 19:29:05 +1000

    Reconnect on close

--- src/p2pQuake/events/error.ts
+++ src/p2pQuake/events/error.ts
@@ -1,5 +1,6 @@
+import { logger } from "../..";
 import { Event } from "../structures/Event";
 
 export default new Event("error", async (err) => {
-    console.error(err);
+    logger.error(err);
 });
